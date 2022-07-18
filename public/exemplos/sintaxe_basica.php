8861cc18685b9459c9adef83bc783692487280e8
pedro <pedro.henrike2812@gmail.com>
2022-07-18 20:58:47 -0300

    Iniciando projeto evento com php

---
+++ public/exemplos/sintaxe_basica.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxe básica PHP</title>
+</head>
+<body>
+    <h1>Exemplo de texto</h1>
+    <?php 
+        echo "<h1>Olá mundo!</h1>";
+        $variavel = 10; // criando variável
+        echo "\n<h4>$variavel</h4>";
+        
+    ?>
+    
+
+</body>
+</html>
