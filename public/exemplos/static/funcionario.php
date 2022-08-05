f52b116f093e1f5a16245faf1ef9e4f615cbd538
pedro <pedro.henrike2812@gmail.com>
2022-08-05 19:14:25 -0300

    finalizado estudo do static

--- public/exemplos/static/funcionario.php
+++ public/exemplos/static/funcionario.php
@@ -6,6 +6,13 @@ class Funcionarioo{
     public static function relatorio($nome, $salario){
         self::$nome = $nome;
         self::$salario = $salario;
-        echo "Olá ".self::$nome ."seu salário é ".self::$salario."reais";
+        echo "Olá ".self::$nome ." seu salário é ".self::$salario." reais";
     }
+    public static function getSalario(){
+        return self::$salario;
+    }
+    public static function setSalario($valor){
+        self::$salario = $valor;
+    }
+    
 }
--- public/exemplos/static/index.php
+++ public/exemplos/static/index.php
@@ -1,4 +1,9 @@
 <?php
 require ("funcionario.php");
-$f1 = new Funcionarioo();
-$f1->relatorio("Paulo", 1200);
+// $f1 = new Funcionarioo();
+// $f1->relatorio("Paulo", 1200);
+
+Funcionarioo::relatorio("Fulano", 1212);
+Funcionarioo::setSalario(1212);
+echo "<br>".Funcionarioo::getSalario();
+
