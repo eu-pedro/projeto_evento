462f3276efec9582cc0577d2b4772e42dbaa9962
pedro <pedro.henrike2812@gmail.com>
2022-08-08 18:53:07 -0300

    atividade

---
+++ public/exemplos/atividade3/Elevador.php
@@ -0,0 +1,44 @@
+<?php 
+class Elevador{
+    private static $andarAtual;
+    private static $totalAndares;
+    private static $capacidadeElevador;
+    private static $quantidadePessoas;
+    private static $resultadoPessoas;
+
+    public static function inicializar($capacidadeElevador, $totalAndares){
+        self::$capacidadeElevador = $capacidadeElevador;
+        self::$totalAndares = $totalAndares;
+        echo "O elevador está iniciando!<br> A capacidade é para ". self::$capacidadeElevador ." pessoas e possui ". self::$totalAndares. "andares!" ;
+    }
+
+    public static function entrar(){
+        self::$resultadoPessoas = self::$quantidadePessoas = 1;
+
+        if(self::$quantidadePessoas > self::$capacidadeElevador){
+            echo "A capacidade máxima do elevador foi atingida!";
+        }
+        else{
+            echo "<br>Uma pessoa entrou no elevador! Total de pessoas no elevador: ". self::$resultadoPessoas += 1;
+        }
+    }
+
+    public static function sair(){
+        if(self::$quantidadePessoas = 0){
+            echo "Não há ninguem dentro do elevador!";
+        }
+        else{
+            echo "Uma pessoa saiu do elevador!";
+        }
+    }
+
+    public static function subir(){
+        if($andarAtual = 5){
+            echo "Você não pode subir mais um andar!";
+        }
+        else{
+
+        }
+    }
+
+}
---
+++ public/exemplos/atividade3/index.php
@@ -0,0 +1,6 @@
+<?php 
+require ("Elevador.php");
+$e1 = new Elevador();
+$e1->inicializar(5,5);
+$e1->entrar();
+$e1->entrar();
