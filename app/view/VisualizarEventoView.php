ae539e9687b1c39828b497b9de8045637939f519
pedro <pedro.henrike2812@gmail.com>
2022-08-22 20:58:33 -0300

    Iniciado outra página de login

---
+++ app/view/VisualizarEventoView.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Gerenciamento de Evento</title>
+</head>
+<body>
+    <h1>Estou na página VisualizarEventoView</h1>
+</body>
+</html>
